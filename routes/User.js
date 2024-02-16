be0f46dd360c32b6dd68fdc62bf916cc27623437
Ezz Addin Kukhun <ezkukhun2000@gmail.com>
2024-02-16 09:44:48 +0200

    user can change his password if he forgot it

--- routes/User.js
+++ routes/User.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const multer = require('multer'); 
-const path = require('path'); 
-const randomstring = require('randomstring'); 
-const sendMail = require('../utils/sendEmail');
-const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+const randomstring = require("randomstring");
+const sendMail = require("../utils/sendEmail");
+const fs = require("fs");
 const userRouter = express.Router();
 const connection = require("../DbConnection");
 const { env } = require("process");
 userRouter.use(express.json());
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'PersonalImgs')
-    },   
-    filename: (req, file, callback)=>{
-        callback(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+  destination: (req, file, callback) => {
+    callback(null, "PersonalImgs");
+  },
+  filename: (req, file, callback) => {
+    callback(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-    }
-})
-
-const upload = multer ({
-    storage: storage, 
-
-})
+const upload = multer({
+  storage: storage,
+});
 
 userRouter.post("/signup", (req, res) => {
   try {
@@ -47,11 +48,17 @@ userRouter.post("/signup", (req, res) => {
     } = req.body;
 
     connection.query("select email, username from users", async (err, rows) => {
-      const person = rows.find((person) => (email == person.email) || (username == person.username));
-      console.log (person)
-      if (person != undefined) return res.status(400).send({message: "There is an error in email or username, try to enter another one"}); 
+      const person = rows.find(
+        (person) => email == person.email || username == person.username
+      );
+      console.log(person);
+      if (person != undefined)
+        return res.status(400).send({
+          message:
+            "There is an error in email or username, try to enter another one",
+        });
 
-      const hashedPassword = await bcrypt.hash(password, 10); 
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       const addNewUserStatement =
         "insert into users (`userId`,`firstName`,`midName`,`lastName`,`birthdate`,`phoneNumber`," +
@@ -80,13 +87,17 @@ userRouter.post("/signup", (req, res) => {
       connection.query(addNewUserStatement, [values], (err) => {
         if (err) res.status(404).send({ message: "failed" });
         else {
-            console.log (process.env.BASE_URL)
-            const mailSubject = "Mail Verification!"; 
-            const randomToken = randomstring.generate(); 
-            const content = `<p> Hi ${firstName + lastName}, Please <a href="${process.env.BASE_URL}/mail-verification?token=${randomToken}">Verify</a> your mail! `
-            sendMail(email, mailSubject, content);
-            connection.query("UPDATE users set token=? where email=?", [randomToken, email])
-            res.status(200).send({ message: "success" });
+          const mailSubject = "Mail Verification!";
+          const randomToken = randomstring.generate();
+          const content = `<p> Hi ${firstName + lastName}, Please <a href="${
+            process.env.BASE_URL
+          }/mail-verification?token=${randomToken}">Verify</a> your mail! `;
+          sendMail(email, mailSubject, content);
+          connection.query("UPDATE users set token=? where email=?", [
+            randomToken,
+            email,
+          ]);
+          res.status(200).send({ message: "success" });
         }
       });
     });
@@ -95,63 +106,102 @@ userRouter.post("/signup", (req, res) => {
   }
 });
 
-userRouter.post ('/login', (req, res)=>{
-    try{
-        const {username, password} = req.body;
-        connection.query(`select username, password from users where username='${username}'`,  async (err, rows)=>{
-            if (rows.length == 0) return res.status(400).send({message: "This is an invalid username!"}); 
-            const isPasswordsMatched = await bcrypt.compare(password, rows[0].password);
-            isPasswordsMatched? res.status(200).send({message: 'success'}) : res.status(400).send({message: 'failed'})
-        });
+userRouter.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+    connection.query(
+      `select username, password from users where username='${username}'`,
+      async (err, rows) => {
+        if (rows.length == 0)
+          return res
+            .status(400)
+            .send({ message: "This is an invalid username!" });
+        const isPasswordsMatched = await bcrypt.compare(
+          password,
+          rows[0].password
+        );
+        isPasswordsMatched
+          ? res.status(200).send({ message: "success" })
+          : res.status(400).send({ message: "failed" });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+userRouter.post(
+  "/uploadPersonalPhoto",
+  upload.single("personalImg"),
+  (req, res) => {
+    const userId = req.body.userId;
+    const photoName = req.file.filename;
+    const getProfilePhotoNameQuery = `select profilePhoto from users where userId='${userId}'`;
+    connection.query(getProfilePhotoNameQuery, (err, rows) => {
+      if (rows[0].profilePhoto != "") {
+        fs.unlinkSync(`./PersonalImgs/${rows[0].profilePhoto}`);
+      }
+    });
+    const uploadProfilePhotoQuery = `update users set profilePhoto ='${photoName}' where userId='${userId}'`;
+    connection.query(uploadProfilePhotoQuery, (err) => {
+      try {
+        return res.status(200).send("success");
+      } catch (err) {
+        return res.status(500).send("failed");
+      }
+    });
+  }
+);
+
+userRouter.get("/userInformation/:id", (req, res) => {
+  const userId = req.params.id;
+  const getUserInformationQuery = `select * from users where userId='${userId}'`;
+  connection.query(getUserInformationQuery, (err, rows) => {
+    try {
+      if (rows.length == 0) {
+        return res.status(400).send("This User Doesn't exist!");
+      } else {
+        return res.status(200).send(rows[0]);
+      }
+    } catch (err) {
+      return res.status(500).send("Error!");
     }
-    catch (err) {
-        console.log (err)
+  });
+});
+
+userRouter.get("/forgetPassword", (req, res) => {
+  const username = req.body.username;
+  connection.query(
+    `SELECT firstName, lastName, email from users where username='${username}'`,
+    (err, rows) => {
+      if (rows.length == 0) {
+        return res.status(404).send({ message: "failed" });
+      } else {
+        const mailSubject = "Change Your Password!";
+        const content = `<p> Hi ${rows[0].firstName + rows[0].lastName}, Please <a href="${
+          process.env.BASE_URL
+        }/change-password?username=${username}">Change Password Now!</a> your mail! `;
+        sendMail(rows[0].email, mailSubject, content);
+        res.status(200).send({ message: "success" });
+      }
     }
-}); 
+  );
+});
+
+userRouter.post ('/changePassword', async (req, res)=>{
+    const {username, password} = req.body; 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    connection.query(`Update users set password='${hashedPassword}' where username='${username}'`, (err)=>{
+      if (err){
+        return res.status(500).send({ message: "it seems that an error occured!" });
+      }
+      else {
+        return res.status(200).send({ message: "Password Updated Successfully, Try to Login!" });
+      }
+    })
 
 
-userRouter.post ('/uploadPersonalPhoto', upload.single('personalImg'), (req, res)=> {
-    const userId = req.body.userId; 
-    const photoName = req.file.filename; 
-    const getProfilePhotoNameQuery = `select profilePhoto from users where userId='${userId}'`
-    connection.query(getProfilePhotoNameQuery , (err, rows)=>{
-        if (rows[0].profilePhoto != ""){
-            fs.unlinkSync(`./PersonalImgs/${rows[0].profilePhoto}`); 
-        }
-    })
-    const uploadProfilePhotoQuery = `update users set profilePhoto ='${photoName}' where userId='${userId}'`
-    connection.query(uploadProfilePhotoQuery, (err)=>{
-        try{
-            return res.status(200).send("success"); 
-        }
-        catch (err){
-            return res.status(500).send("failed"); 
-        }
-    })
-}); 
 
-userRouter.get('/userInformation/:id', (req, res)=>{
-    const userId = req.params.id; 
-    const getUserInformationQuery = `select * from users where userId='${userId}'`;
-    connection.query(getUserInformationQuery , (err, rows)=>{
-        try{
-            if (rows.length == 0){
-                return res.status(400).send("This User Doesn't exist!"); 
-            }
-            else {
-                return res.status(200).send(rows[0]); 
-            }
-        }
-        catch (err){
-            return res.status(500).send("Error!"); 
-        }
-    })
 })
 
-
-
-
-
-
 module.exports = userRouter;
-
--- routes/webRouter.js
+++ routes/webRouter.js
@@ -8,15 +8,18 @@ userRouter.set ('views', './views');
 
 userRouter.get ('/mail-verification', (req, res) => {
     const token = req.query.token;
-    
-    console.log ("token is: " + token)
-
     connection.query ('SELECT * from users where token=? ', token, (err, result, fields)=> {
         if (result.length > 0){
             connection.query (`UPDATE users set token=null, isVerified=1 where userId = '${result[0].userId}'`); 
             return res.render ('mail-verification', {message: 'Mail Verified Successfully!'})
         }
     })
+}); 
+
+
+userRouter.get ('/change-password', (req, res) => {
+    const username = req.query.username;
+    return res.render ('change-password', {message: "TTT"})    
 }); 
 
 module.exports = userRouter; 
